04458b644653e9f593b90a30492947d151b2a381
mgarciate <mgarciate@gmail.com>
2024-11-15 18:47:19 +0100

    Create iOS Project

---
+++ ios/sirichain-ios/sirichain-ios/sirichain_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  sirichain_iosApp.swift
+//  sirichain-ios
+//
+//  Created by mgarciate on 15/11/24.
+//
+
+import SwiftUI
+
+@main
+struct sirichain_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
