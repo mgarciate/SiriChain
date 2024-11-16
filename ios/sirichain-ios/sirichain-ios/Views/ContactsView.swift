966cb313b8b891c7e90006041ce292b056b90d10
mgarciate <mgarciate@gmail.com>
2024-11-16 07:02:57 +0100

    Show Contacts

--- ios/sirichain-ios/sirichain-ios/Views/ContactsView.swift
+++ ios/sirichain-ios/sirichain-ios/Views/ContactsView.swift
@@ -6,10 +6,46 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct ContactsView: View {
+//    @Query var contacts: [Contact]
+    @State var contacts: [Contact] = Contact.dummyData
+    
     var body: some View {
-        Text("Contacts")
+        NavigationStack {
+            ZStack {
+                if contacts.isEmpty {
+                    ContentUnavailableView("No contacts", systemImage: "person.2", description: Text("No contacts available. Tap the + button in the top-right corner to add a new contact."))
+                } else {
+                    List {
+                        ForEach(contacts) { contact in
+                            NavigationLink {
+                                Text(contact.name)
+                            } label: {
+                                Text(contact.name)
+                            }
+                        }
+                    }
+                    .listStyle(.plain)
+                }
+            }
+            .navigationTitle("Contacts")
+            .toolbar {
+                ToolbarItem(placement: .primaryAction) {
+                    Button {
+                        // TODO: Create a new contact
+                    } label: {
+                        Image(systemName: "plus")
+                    }
+                }
+            }
+        }
+        .onAppear() {
+            if contacts.isEmpty {
+                // TODO: Insert default values
+            }
+        }
     }
 }
 
